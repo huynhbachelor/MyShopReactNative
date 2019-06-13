590191159788e8e95bd3dca1dd244863a1294a28
huynhbachelor <xuanbach.1994@gmail.com>
2019-06-13 14:01:04 +0700

    element header searchbar

--- src/components/Mains/Main.js
+++ src/components/Mains/Main.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import { View, Text, SafeAreaView, ScrollView, Dimensions } from 'react-native';
+import React from 'react';
+import { View, SafeAreaView, ScrollView, ActivityIndicator } from 'react-native';
 import { createDrawerNavigator, createAppContainer, DrawerItems } from 'react-navigation';
+import { Avatar } from 'react-native-elements';
 import Shop from './Shops/Shop';
 import Authencation from '../Authentications/Authentication';
 import ChangeInfor from '../ChangeInfors/ChangeInfor';
@@ -9,8 +10,22 @@ import OrderHistory from '../OrderHistorys/OrderHistory';
 
 const CustomDrawerComponent = (props) => (
   <SafeAreaView style={{ flex: 1 }} forceInset={{ top: 'always', horizontal: 'never' }}>
-    <View style={{ height: 150, backgroundColor: 'gray' }}>
-      <Text>Infor</Text>
+    <View 
+      style={{ 
+        height: 150, 
+        backgroundColor: 'white', 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+      }}
+    >
+      <Avatar 
+        rounded
+        size='large'
+        source={{ uri: 'https://lh3.googleusercontent.com/-Ywt4bmaESdQ/V-aBCJhAVJI/AAAAAAAAASc/IOwD0F8k0zkz4nLjUSRJGJSKEb6VMFepwCEwYBhgL/w140-h139-p/jannina_w.jpg' }}
+        PlaceholderContent={<ActivityIndicator />}
+        showEditButton
+        title='bach huynh'
+      />
     </View>
     <ScrollView>
       <DrawerItems {...props} />
--- src/components/Mains/Shops/Home/Home.js
+++ src/components/Mains/Shops/Home/Home.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Header, SearchBar } from 'react-native-elements';
 
 class HomeScreen extends Component {
+    state = {
+        search: '',
+    };
+    
+    updateSearch = search => {
+        this.setState({ search });
+    };
+
     render() {
         return (
             <View>
-                <Text>HomeScreen</Text>
+                <Header 
+                    leftComponent={{ 
+                        icon: 'menu', 
+                        color: 'white',
+                        onPress: () => { this.props.navigation.openDrawer(); }
+                    }}
+                    centerComponent={{ text: 'Shop', style: { color: 'white', fontSize: 20 } }}
+                    rightComponent={{
+                        icon: 'apps',
+                        color: 'white'
+                    }}
+                    containerStyle={{
+                        height: 50,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        paddingBottom: 20
+                    }}
+                />               
+                <SearchBar 
+                    placeholder="Type Here..."
+                    lightTheme
+                    round
+                    onChangeText={this.updateSearch}
+                    value={this.state.search}
+                />
             </View>
         );
     }
